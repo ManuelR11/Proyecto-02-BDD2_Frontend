ea8a76c9d6a85d3e5352f4e1fd3301aa953fc8b9
ManuelR11 <rod21509@uvg.edu.gt>
2024-04-14 13:59:48 -0600

    Inicio de Proyecto

---
+++ proyecto_02/src/views/Home/home.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./home.css";
+
+
+function Home() {
+    return (    
+        <div className="home">
+            <h1>Home</h1>
+        </div>
+    );
+}
+
+export default Home;
